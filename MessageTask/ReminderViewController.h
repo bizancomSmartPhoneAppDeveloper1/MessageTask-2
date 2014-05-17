190ee559e8931acd5c6e6f9ce99058faf95e0ab6
TomomiTano <Apple@Apple-no-MacBook-Air.local>
2014-05-17 10:56:10 +0900

    test

---
+++ MessageTask/ReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderViewController.h
+//  MessageTask
+//
+//  Created by bizan.com.mac04 on 2014/05/17.
+//  Copyright (c) 2014年 com.bizan.kunren1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderViewController : UIViewController
+
+@end
